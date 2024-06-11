86342ab9a670d6d98dfca4acc624fd12d3da73eb
unknown <amaan2228web@gmail.com>
2024-06-11 17:58:33 +0530

    redux store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,9 @@ import { configureStore, createStore } from "@reduxjs/toolkit";
 import cakeReducer from "./cakes/CakeReducer";
 import rootReducers from "./rootReducer";
 import logger from "redux-logger";
-import {composeWithDevTools} from 'redux-devtools-extension'
 
 const store = configureStore({reducer: rootReducers,
     middleware: (getInitialState) => getInitialState().concat(logger),
-    devTools: process.env.NODE_ENV !== 'production',
 })
 
 export default store

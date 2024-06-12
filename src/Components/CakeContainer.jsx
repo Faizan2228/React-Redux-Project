b4ff64faab360cd80c484c4466994b3eee75f984
unknown <amaan2228web@gmail.com>
2024-06-12 13:34:52 +0530

    updated cake container jsx file

--- src/Components/CakeContainer.jsx
+++ src/Components/CakeContainer.jsx
@@ -6,7 +6,7 @@ const CakeContainer = (props) => {
   return (
     <div>
         <h2>Number of cakes - {props.numOfCakes}</h2>
-        <button onClick={props.buyCake}>Buy cake</button>
+        <button onClick={props.buyCake}>Click to Buy cake</button>
     </div>
     
   )
